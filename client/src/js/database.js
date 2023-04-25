382331046fc884637a4e5d849026a34ed8bd630a
George S <reneehe@George-Sem-2.local>
2023-04-25 16:49:09 -0700

    update database js file logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  const contactDb = await openDB('contact', 1);
+  const tx = contactDb.transaction('contact', 'readwrite');
+  const store = tx.objectStore('contact');
+  const request = store.add(content); //is this correct or shold the method be differe?
+
+  const result = await request;
+  console.log('🚀 - data updated in the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const contactDb = await openDB('contact', 1);
+  const tx = contactDb.transaction('contact', 'readonly');
+  const store = tx.objectStore('contact');
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
